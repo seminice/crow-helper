6ecb0a6eb9b016cb303c5fc7e1cd432ea385e983
Luksy Omar <luksyme@gmail.com>
2014-11-22 11:28:23 -0800

    Initial commit

---
+++ helper.js
@@ -0,0 +1,76 @@
+( function( window ) {
+'use strict';
+
+// Gets a list of the classes of an element and make a list with a space at
+// the end of each class name to ease finding.
+// @param   elem    HTML element
+// @return  string  list of class names
+function getClasses(elem) {
+    return (' ' + (elem.className || '') + ' ').replace(/\s+/gi, ' ');
+}
+
+// Checks if an element has a class
+// @param   elem     HTML element
+// @param   cls      class name
+// @return  boolean
+function hasClass(elem, cls) {
+    var list = typeof elem == 'string' ? elem : getClasses(elem);
+    return list.indexOf(' ' + cls + ' ') >= 0;
+}
+
+// Adds a class to an element
+// @param   elem     HTML element
+// @param   cls      class name
+function addClass(elem, cls) {
+    var oldClasses = getClasses(elem);
+    var newClasses = oldClasses + cls;
+
+    // Return if the element already has the class
+    if (hasClass(oldClasses, cls)) return;
+
+    // Remove the extra space at the begining
+    elem.className = newClasses.substring(1);
+}
+
+// Removes a class from an element
+// @param   elem     HTML element
+// @param   cls      class name
+function removeClass(elem, cls) {
+    var oldClasses = getClasses(elem);
+    var newClasses;
+
+    // Return if the element doesnt has the class
+    if (!hasClass(elem, cls)) return;
+
+    // Replace the class name.
+    newClasses = oldClasses.replace(' ' + cls + ' ', ' ');
+
+    // Remove the extra space at the begining and end
+    elem.className = newClasses.substring(1, newClasses.length - 1);
+}
+
+// Toggles a class on an element
+// @param   elem     HTML element
+// @param   cls      class name
+function toggleClass( elem, cls ) {
+  var fn = hasClass( elem, cls ) ? removeClass : addClass;
+  fn(elem, cls);
+}
+
+
+var utils = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+window.utils = utils;
+
+})( window );
